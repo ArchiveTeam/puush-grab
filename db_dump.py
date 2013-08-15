9838e7ccaa269be6487ab0756829857d4efcbfd0
Christopher Foo <chris.foo@gmail.com>
2013-08-15 01:02:26 -0400

    db_dump.py: Support reading an archived log dump.

--- db_dump.py
+++ db_dump.py
@@ -12,24 +12,34 @@ def main():
     sub_parsers = arg_parser.add_subparsers(title='Command',
         dest='command')
 
-    arg_parser.add_argument('project', help='Name of the project')
     arg_parser.add_argument('--host', help='Hostname of database server',
         default='localhost')
     arg_parser.add_argument('--port', help='Port number of server',
         default=6379, type=int)
     arg_parser.add_argument('--db', help='Database number',
         default=0, type=int)
-    
+
     done_arg_parser = sub_parsers.add_parser('done',
         help='Dump out done items')
+    done_arg_parser.add_argument('project', help='Name of the project')
     done_arg_parser.set_defaults(func=done_command)
-    
+
     log_arg_parser = sub_parsers.add_parser('log',
         help='Dump out log with privacy')
     log_arg_parser.add_argument('--scrub-username', action='store_true',
         help='Scrub out the usernames as well')
+    log_arg_parser.add_argument('project', help='Name of the project')
     log_arg_parser.set_defaults(func=log_command)
-    
+
+    archived_log_arg_parser = sub_parsers.add_parser('archivedlog',
+        help='Dump out archived log with privacy')
+    archived_log_arg_parser.add_argument('--scrub-username',
+        action='store_true',
+        help='Scrub out the usernames as well')
+    archived_log_arg_parser.add_argument('log_file',
+        help='Path of log file')
+    archived_log_arg_parser.set_defaults(func=archived_log_command)
+
     args = arg_parser.parse_args()
     args.func(args)
 
@@ -44,17 +54,17 @@ def get_expanded_item_name(item_name):
     else:
         start_item = item_name
         end_item = item_name
-    
+
     start_num = base62_decode(start_item)
     end_num = base62_decode(end_item)
-    
+
     for num in xrange(start_num, end_num + 1):
         yield base62_encode(num)
 
 
 def done_command(args):
     r = get_redis_connection(args)
-    
+
     for item in r.smembers('%s:done' % args.project):
         for expanded_item_name in get_expanded_item_name(item):
             print(expanded_item_name)
@@ -64,25 +74,39 @@ def log_command(args):
     r = get_redis_connection(args)
     fetch_size = 10000
     i = 0
-    
+
     while True:
         l = r.lrange('%s:log' % args.project, i, i + fetch_size - 1)
-        
+
         if not l:
             break
-        
+
         for item in l:
             doc = json.loads(item)
             doc['ip'] = '<scrubbed>'
             id_doc = json.loads(doc['id'])
             doc['id'] = id_doc
-            
+
             if args.scrub_username:
                 doc['by'] = '<scrubbed>'
-            
+
             print(json.dumps(doc))
-            
+
         i += fetch_size
+
+
+def archived_log_command(args):
+    with open(args.log_file, 'rt') as f:
+        for item in f:
+            doc = json.loads(item)
+            doc['ip'] = '<scrubbed>'
+            id_doc = json.loads(doc['id'])
+            doc['id'] = id_doc
+
+            if args.scrub_username:
+                doc['by'] = '<scrubbed>'
+
+            print(json.dumps(doc))
 
 
 if __name__ == '__main__':
